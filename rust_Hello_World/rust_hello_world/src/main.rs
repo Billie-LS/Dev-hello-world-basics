321cd16e6bf84c27d1379c545229b0d0c746cd07
Billie-LS <lskylizard@yahoo.com>
2024-01-06 16:29:43 -0600

    Rust hello-world

--- rust_Hello_World/rust_hello_world/src/main.rs
+++ rust_Hello_World/rust_hello_world/src/main.rs
@@ -1,3 +1,5 @@
+// Basic 'first Rust program' that prints a greeting message to the console.
 fn main() {
+    // The println! macro is used to display/print to console the output, the string "Hello, World!"
     println!("Hello, world!");
 }
