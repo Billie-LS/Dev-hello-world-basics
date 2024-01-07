5f36f9bc05e3230f82756cecc4c364edec8db21f
Billie-LS <lskylizard@yahoo.com>
2024-01-06 18:36:46 -0600

    Java hello world!

---
+++ java_helloWorld/HelloWorld.java
@@ -0,0 +1,11 @@
+// Basic 'first Java program' that prints a greeting message to the console.
+
+// The class HelloWorld is the entry point of the program.
+public class HelloWorld {
+    // The main method is the starting point of execution.
+    public static void main(String[] args) {
+        // The System.out.println() statement is used to display output.
+        // In this case, it prints the string "Hello, World!" to the console.
+        System.out.println("Hello, World!");
+    }
+}
