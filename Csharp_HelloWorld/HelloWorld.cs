3117b0a26dc99b1b939702085c6c9a420a1f20ae
Billie-LS <lskylizard@yahoo.com>
2024-01-06 18:59:12 -0600

    C# HelloWorld

---
+++ Csharp_HelloWorld/HelloWorld.cs
@@ -0,0 +1,13 @@
+using System;
+// Basic 'first C# program' that prints a greeting message to the console.
+// The class HelloWorld is the entry point of the program.
+class HelloWorld
+{
+    // The Main method is the starting point of execution.
+    static void Main()
+    {
+        // The Console.WriteLine() statement is used to display output.
+        // In this case, it prints the string "Hello, World!" to the console.
+        Console.WriteLine("Hello, World!");
+    }
+}
